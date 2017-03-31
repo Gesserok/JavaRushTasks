0c146356215444d05955b09bfb8674e084ae1234
Gesserok <fikpi@voliacable.com>
2017-03-31 15:52:20 +0300

    1

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -27,15 +27,15 @@ public class Solution {
 
                 while (inputStreamReader.ready()) {
                     int count = inputStreamReader.read(buffer);
+                    char[] bf = buffer.clone();
                     if (!String.valueOf(buffer).startsWith(sb.toString())) {
-                        charList.add(buffer);
-                        System.out.println(buffer);
+                        charList.add(bf);
+                        System.out.println(bf);
                     }
                 }
 
-
-
                 inputStreamReader.close();
+
                 OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "Unicode");
                 for (char[] x : charList) {
                     osw.write(x);
@@ -53,72 +53,65 @@ public class Solution {
 
                 InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "Unicode");
                 char[] buffer = new char[52];
-                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("E:\\12.txt"), "Unicode");
                 boolean st = false;
                 while (inputStreamReader.ready()) {
                     int count = inputStreamReader.read(buffer);
+                    char[] bf = buffer.clone();
                     if (!String.valueOf(buffer).startsWith(sb)) {
-                        osw.write(buffer);
+                        charList.add(bf);
                     } else {
                         int i;
                         int j =8;
                         for (i = 0; i < args[2].toCharArray().length; i++) {
-                            buffer[j] = args[2].toCharArray()[i];
+                            bf[j] = args[2].toCharArray()[i];
                             j++;
                         }
                         if (j !=37 ) {
                             for (j = j; j < 38; j++) {
-                                buffer[j] = ' ';
+                                bf[j] = ' ';
                             }
                         }
                         for (i = 0; i < args[3].toCharArray().length; i++) {
-                            buffer[j] = args[3].toCharArray()[i];
+                            bf[j] = args[3].toCharArray()[i];
                             j++;
                         }
-                            if (j != 45) {
-                                for (j = j; j < 46; j++) {
-                                    buffer[j] = ' ';
-                                }
+                        if (j != 45) {
+                            for (j = j; j < 46; j++) {
+                                bf[j] = ' ';
                             }
+                        }
 
 
                         for (i = 0; i < args[4].toCharArray().length; i++) {
-                            buffer[j] = args[4].toCharArray()[i];
-                                j++;
+                            bf[j] = args[4].toCharArray()[i];
+                            j++;
+                        }
+                        if (j != 49) {
+                            for (j = j; j < 50; j++) {
+                                bf[j] = ' ';
                             }
-                            if (j != 49) {
-                                for (j = j; j < 50; j++) {
-                                    buffer[j] = ' ';
-                                }
-                            }
+                        }
 
 
 
-                        osw.write(buffer);
+                        charList.add(bf);
 
                     }
 
                 }
                 inputStreamReader.close();
-                osw.close();
 
-                InputStreamReader copyStream = new InputStreamReader(new FileInputStream("E:\\12.txt"), "Unicode");
-                OutputStreamWriter copyOsw = new OutputStreamWriter(new FileOutputStream(file), "Unicode");
-
-                while (copyStream.ready()) {
-                    int count = copyStream.read(buffer);
-                        copyOsw.write(buffer);
+                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "Unicode");
+                for (char[] x : charList) {
+                    osw.write(x);
+                    System.out.println(x);
                 }
-                copyStream.close();
-                copyOsw.close();
+
+                osw.close();
 
             }
         }
 
-        File fileName = new File("E:\\12.txt");
-        if (fileName.exists()) {
-            fileName.delete();
-        }
 
     }
 
@@ -126,23 +119,23 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         if (!(args.length == 0)) {
 
-                char[] chars = new char[0];
-                int[] intsLine = new int[0];
-                int[] id = new int[8];
+            char[] chars = new char[0];
+            int[] intsLine = new int[0];
+            int[] id = new int[8];
 
-                    for (int i = 0; i < id.length; i++) {
-                        if (i < args[1].toCharArray().length) {
-                            char[] idChars = args[1].toCharArray();
-                            id[i] = (int) ((args[1].toCharArray())[i]);
-                        } else {
-                            id[i] = 32;
-                        }
-                    }
-
-
-                for (int x : id) {
-                    sb.append((char) x);
+            for (int i = 0; i < id.length; i++) {
+                if (i < args[1].toCharArray().length) {
+                    char[] idChars = args[1].toCharArray();
+                    id[i] = (int) ((args[1].toCharArray())[i]);
+                } else {
+                    id[i] = 32;
                 }
+            }
+
+
+            for (int x : id) {
+                sb.append((char) x);
+            }
         }
         return sb.toString();
     }
