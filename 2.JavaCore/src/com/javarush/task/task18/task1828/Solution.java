2232b2c3e3eab570789b7f242fa67cd6254f824f
Gesserok <fikpi@voliacable.com>
2017-04-04 21:11:54 +0300

    123

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -9,135 +9,84 @@ import java.util.ArrayList;
 
 public class Solution {
 
-    public static ArrayList<String> stringList = new ArrayList<String>();
-    public static ArrayList<char[]> charList = new ArrayList<char[]>();
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String file = reader.readLine();
-
-
-
+        BufferedReader fileNameReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = fileNameReader.readLine();
         if (!(args.length == 0)) {
+            char[] idInFile = new char[52];
+            ArrayList<String> arrayList = new ArrayList<String>();
+            String id = args[1];
+            while (id.length() < 8) {
+                id = id.concat(" ");
+            }
+
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(fileName), "Unicode");
+
+
             if (args[0].equals("-d")) {
+                while (isr.ready()) {
+                    int count = isr.read(idInFile);
+                    String lineInFile = String.copyValueOf(idInFile);
 
-                String sb = argsParse(args);
-                InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "Unicode");
-                char[] buffer = new char[52];
-
-                while (inputStreamReader.ready()) {
-                    int count = inputStreamReader.read(buffer);
-                    char[] bf = buffer.clone();
-                    if (!String.valueOf(buffer).startsWith(sb.toString())) {
-                        charList.add(bf);
-                        System.out.println(bf);
+                    if (!lineInFile.startsWith(id)) {
+                        arrayList.add(lineInFile);
                     }
                 }
-
-                inputStreamReader.close();
-
-                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "Unicode");
-                for (char[] x : charList) {
+                isr.close();
+                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(fileName), "Unicode");
+                for (String x : arrayList) {
                     osw.write(x);
-                    System.out.println(x);
                 }
-
                 osw.close();
-
-
-
             }
+
+
+
             if (args[0].equals("-u")) {
 
-                String sb = argsParse(args);
+                String productName = args[2];
+                while (productName.length() < 30) {
+                    productName = productName.concat(" ");
+                }
 
-                InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "Unicode");
-                char[] buffer = new char[52];
-                boolean st = false;
-                while (inputStreamReader.ready()) {
-                    int count = inputStreamReader.read(buffer);
-                    char[] bf = buffer.clone();
-                    if (!String.valueOf(buffer).startsWith(sb)) {
-                        charList.add(bf);
+                String price = args[3];
+                while (price.length() < 8) {
+                    price = price.concat(" ");
+                }
+
+                String quantity = args[4];
+                while (quantity.length() < 4) {
+                    quantity = quantity.concat(" ");
+                }
+
+
+                while (isr.ready()) {
+                    int count = isr.read(idInFile);
+                    String lineInFile = String.copyValueOf(idInFile);
+
+                    if (!lineInFile.startsWith(id)) {
+                        arrayList.add(lineInFile);
                     } else {
-                        int i;
-                        int j =8;
-                        for (i = 0; i < args[2].toCharArray().length; i++) {
-                            bf[j] = args[2].toCharArray()[i];
-                            j++;
-                        }
-                        if (j !=37 ) {
-                            for (j = j; j < 38; j++) {
-                                bf[j] = ' ';
-                            }
-                        }
-                        for (i = 0; i < args[3].toCharArray().length; i++) {
-                            bf[j] = args[3].toCharArray()[i];
-                            j++;
-                        }
-                        if (j != 45) {
-                            for (j = j; j < 46; j++) {
-                                bf[j] = ' ';
-                            }
-                        }
-
-
-                        for (i = 0; i < args[4].toCharArray().length; i++) {
-                            bf[j] = args[4].toCharArray()[i];
-                            j++;
-                        }
-                        if (j != 49) {
-                            for (j = j; j < 50; j++) {
-                                bf[j] = ' ';
-                            }
-                        }
-
-
-
-                        charList.add(bf);
-
+                        arrayList.add(id.concat(productName).concat(price).concat(quantity));
                     }
-
                 }
-                inputStreamReader.close();
+                isr.close();
+                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(fileName), "Unicode");
+                for (String x : arrayList) {
 
-                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "Unicode");
-                for (char[] x : charList) {
-                    osw.write(x);
-                    System.out.println(x);
+                    if (x.startsWith(id)) {
+                        osw.write(x);
+                        osw.append((char) 13);
+                        osw.append((char) 10);
+                    } else {
+                        osw.write(x);
+                    }
                 }
-
                 osw.close();
-
             }
         }
-
-
     }
 
-    public static String argsParse (String[] args) {
-        StringBuilder sb = new StringBuilder();
-        if (!(args.length == 0)) {
-
-            char[] chars = new char[0];
-            int[] intsLine = new int[0];
-            int[] id = new int[8];
-
-            for (int i = 0; i < id.length; i++) {
-                if (i < args[1].toCharArray().length) {
-                    char[] idChars = args[1].toCharArray();
-                    id[i] = (int) ((args[1].toCharArray())[i]);
-                } else {
-                    id[i] = 32;
-                }
-            }
-
-
-            for (int x : id) {
-                sb.append((char) x);
-            }
-        }
-        return sb.toString();
-    }
 }
-
